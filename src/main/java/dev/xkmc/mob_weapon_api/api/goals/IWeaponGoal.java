c0b7e6386c84c238366e39df9efe77f3694081c5
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-04-26 18:30:21 +0800

    fix range check

--- src/main/java/dev/xkmc/mob_weapon_api/api/goals/IWeaponGoal.java
+++ src/main/java/dev/xkmc/mob_weapon_api/api/goals/IWeaponGoal.java
@@ -5,13 +5,26 @@ import net.minecraft.world.item.ItemStack;
 
 public interface IWeaponGoal<E extends Mob> {
 
+	/**
+	 * Returns true if golem can switch to this goal
+	 * */
 	default boolean mayActivate(ItemStack stack) {
 		return true;
 	}
 
+	/**
+	 * Returns true when this ranged goal can be switched to after golem is far enough from target.
+	 * */
+	default boolean isAvailable(ItemStack stack) {
+		return mayActivate(stack);
+	}
+
 	double range(ItemStack stack);
 
-	default boolean shouldUseForMelee() {
+	/**
+	 * Return true if golem can use stack associated with this goal for melee attack.
+	 * */
+	default boolean shouldUseForMelee(ItemStack other) {
 		return true;
 	}
 
