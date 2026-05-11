3793b6e84f6314b65e7200c8af0a7f9ceabffe52
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-05-11 20:43:27 +0800

    containr fix

--- src/main/java/dev/xkmc/cataclysm_mux_0327/CataclysmMux_0327.java
+++ src/main/java/dev/xkmc/cataclysm_mux_0327/CataclysmMux_0327.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(CataclysmMux_0327.MODID)
 public class CataclysmMux_0327 {
 
-	public static final String MODID = "cataclysm_mux";
+	public static final String MODID = "cataclysm_mux_0327";
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
