d05f6ed2c2f0b50885dcb9b06c220e248467670a
Dr-WeiAL <931311372@qq.com>
2026-05-12 19:27:35 +0800

    0316

---
+++ src/main/java/dev/xkmc/cataclysm_mux_0316/CataclysmMux_0316.java
@@ -0,0 +1,14 @@
+package dev.xkmc.cataclysm_mux_0316;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(CataclysmMux_0316.MODID)
+public class CataclysmMux_0316 {
+
+	public static final String MODID = "cataclysm_mux_0316";
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+}
